0d08c51658ab66d46ae77501f68534b5a79183e0
edwnbr <sadovskijzena57@gmail.com>
2025-07-20 14:05:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,7 @@ from telegram.ext import (
 )
 
 # ===================== CONFIG =====================
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-if not BOT_TOKEN:
-    raise ValueError("❌ BOT_TOKEN not set in environment!")
+BOT_TOKEN = os.environ.get("BOT_TOKEN") or "7697812728:AAG72LwVSOhN-v1kguh3OPXK9BzXffJUrYE"  # <-- вставь сюда свой токен
 
 WEBHOOK_HOST = f'https://{os.environ.get("https://btm-c4tt.onrender.com")}'
 WEBHOOK_PATH = f"/{BOT_TOKEN}"
